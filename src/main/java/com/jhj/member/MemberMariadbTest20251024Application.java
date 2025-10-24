60bf88dc91c1bc1a13906c6692d3cc6f0cfd4a89
wjsgudwnswjsgudwns <wns10099@naver.com>
2025-10-24 14:32:27 +0900

    Initial commit

---
+++ src/main/java/com/jhj/member/MemberMariadbTest20251024Application.java
@@ -0,0 +1,13 @@
+package com.jhj.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberMariadbTest20251024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberMariadbTest20251024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhj/member/MemberMariadbTest20251024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhj.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberMariadbTest20251024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
